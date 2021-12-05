a25178b0764141414d70a0056778c6e8118d5b18
Diego <diego.nicolas.gandara@gmail.com>
2021-12-05 19:05:37 -0300

    At our App.ts, import that object configuration and write some boilerplate code to configure swagger with swaggerJSDoc and express-ui. Also, configure the /docs endpoint.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express from 'express';
 import routes from './routes';
 import morgan from 'morgan';
 import cors from 'cors';
+import { options } from './docs/swagger';
+
+// Swagger
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+const specs = swaggerJSDoc(options);
 
 const app = express();
 
@@ -11,5 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 export default app;
