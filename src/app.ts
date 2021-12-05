3667d080e2eef7b70e8524cc14bd3991e1f6e40b
Diego <diego.nicolas.gandara@gmail.com>
2021-12-05 00:56:20 -0300

    Just for fun, try a new way of seting app server.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import routes from './routes';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,7 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+app.set('port', process.env.PORT || 3000);
+const PORT = app.get('port');
 
 app.listen(PORT, () => {
   console.log('Server listening on port', PORT);
