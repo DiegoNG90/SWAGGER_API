9e1e4be1ed80f6922dbe8863299b1355e5b755a6
Diego <diego.nicolas.gandara@gmail.com>
2021-12-05 19:07:13 -0300

    Finally, at our /routes/task.ts file, we start documentating with Swagger. First, define a Task schema component, after a Tag to move all routes associated with 'Tasks' and finally start configuring each route. For now, only configured two GET routes: getTasks and countTasks.

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -2,8 +2,67 @@ import { Router } from 'express';
 import { getTasks, createTask, getTask, deleteTask, updateTask, countTasks } from '../controllers/tasks';
 
 const router = Router();
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Task:
+ *     type: object
+ *     properties:
+ *        id:
+ *          type: number
+ *          description: id of the task
+ *        title:
+ *          type: string
+ *          description: title of the task
+ *        description:
+ *            type: string
+ *            description: description of the task
+ *     required:
+ *        -title
+ *        -description
+ *     example:
+ *        id: G7RDurZJALB7rGEdWBxU8
+ *        title: "Task 1"
+ *        description: "This is the first task"
+ *
+ */
 
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: Tasks management
+ */
+
+/**
+ * @swagger
+ * /tasks/all:
+ *  get:
+ *    tags: [Tasks]
+ *    summary: Returns an array of all tasks
+ *    responses:
+ *      200:
+ *        description: An array of tasks
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Task'
+ */
 router.get('/all', getTasks);
+
+/**
+ * @swagger
+ * /tasks/count:
+ *  get:
+ *    tags: [Tasks]
+ *    summary: Returns the total number of tasks
+ *    responses:
+ *      200:
+ *       description: The total number of tasks
+ */
 
 router.get('/count', countTasks);
 
